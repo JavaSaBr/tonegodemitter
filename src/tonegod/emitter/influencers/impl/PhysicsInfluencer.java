0130d2f66f13b0f9f4118982af26f88dde293fb1
javasabr <javasabr@gmail.com>
2017-01-09 05:23:57 +0300

    optimizied the physics influencer

--- src/tonegod/emitter/influencers/impl/PhysicsInfluencer.java
+++ src/tonegod/emitter/influencers/impl/PhysicsInfluencer.java
@@ -19,6 +19,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
+import tonegod.emitter.ParticleEmitterNode;
 import tonegod.emitter.influencers.ParticleInfluencer;
 import tonegod.emitter.particle.ParticleData;
 
@@ -45,57 +46,79 @@ public class PhysicsInfluencer extends AbstractParticleInfluencer {
     /**
      * The list of collidable geometries.
      */
+    @NotNull
     private final GeometryList geometries;
 
     /**
      * The list of temprary collidable geometries.
      */
+    @NotNull
     private final GeometryList tempGeometries;
 
     /**
      * The quad.
      */
+    @NotNull
     private final Quad quad;
 
     /**
      * The geometry.
      */
+    @NotNull
     private final Geometry geom;
+
+    /**
+     * The contact surface.
+     */
+    @NotNull
+    private final Triangle contactSurface;
 
     /**
      * The quanterion.
      */
+    @NotNull
     private final Quaternion quat;
 
     /**
      * The collision results.
      */
+    @NotNull
     private final CollisionResults results;
-
-    /**
-     * The collision result.
-     */
-    private CollisionResult result;
-
-    /**
-     * The contact surface.
-     */
-    private Triangle contactSurface;
 
     /**
      * The reflect.
      */
+    @NotNull
     private final Vector3f reflect;
 
     /**
      * The two.
      */
+    @NotNull
     private final Vector3f two;
 
     /**
      * The normal.
      */
+    @NotNull
     private final Vector3f normal;
+
+    /**
+     * The temp vector.
+     */
+    @NotNull
+    private final Vector3f tempVec;
+
+    /**
+     * The temp vector #2.
+     */
+    @NotNull
+    private final Vector3f tempVec2;
+
+    /**
+     * The collision result.
+     */
+    private CollisionResult result;
 
     /**
      * The collision reaction.
@@ -132,9 +155,12 @@ public class PhysicsInfluencer extends AbstractParticleInfluencer {
         this.reflect = new Vector3f();
         this.two = new Vector3f();
         this.normal = new Vector3f();
+        this.tempVec = new Vector3f();
+        this.tempVec2 = new Vector3f();
         this.collisionReaction = CollisionReaction.BOUNCE;
         this.collisionThreshold = 0.1f;
         this.restitution = 0.5f;
+        this.contactSurface = new Triangle();
         geom.setMesh(quad);
         quad.updateBound();
         geom.updateModelBound();
@@ -169,41 +195,52 @@ public class PhysicsInfluencer extends AbstractParticleInfluencer {
      * @param tpf          the tpf.
      */
     private void findCollisions(final @NotNull ParticleData particleData, final float tpf) {
+
+        final ParticleEmitterNode emitterNode = particleData.getEmitterNode();
+
         for (int i = 0; i < geometries.size(); i++) {
             final Geometry geometry = geometries.get(i);
             try {
 
-                if (results.size() != 0) results.clear();
+                if (results.size() != 0) {
+                    results.clear();
+                }
 
                 updateCollisionShape(particleData, tpf);
 
                 geometry.collideWith(geom.getWorldBound(), results);
 
                 if (results.size() > 0) {
+
+                    final Vector3f velocity = particleData.velocity;
+
                     result = results.getClosestCollision();
+
                     switch (collisionReaction) {
                         case BOUNCE: {
-                            contactSurface = result.getTriangle(null);
-                            contactSurface.calculateNormal();
+                            result.getTriangle(contactSurface);
                             normal.set(contactSurface.getNormal());
-                            twoDot = 2.0f * particleData.velocity.dot(normal);
+                            twoDot = 2.0f * velocity.dot(normal);
                             two.set(twoDot, twoDot, twoDot);
-                            reflect.set(two.mult(normal).subtract(particleData.velocity)).negateLocal().normalizeLocal();
-                            len = particleData.velocity.length() * (restitution - 0.1f) + (FastMath.nextRandomFloat() * 0.2f);
-                            particleData.velocity.set(reflect).multLocal(len);
+                            reflect.set(two.mult(normal, tempVec)
+                                    .subtract(velocity, tempVec2))
+                                    .negateLocal().normalizeLocal();
+                            len = velocity.length() * (restitution - 0.1f) + (FastMath.nextRandomFloat() * 0.2f);
+                            velocity.set(reflect).multLocal(len);
                             particleData.collision = true;
                             break;
                         }
                         case STICK: {
-                            particleData.velocity.set(0, 0, 0);
+                            velocity.set(0, 0, 0);
                             break;
                         }
                         case DESTROY: {
-                            particleData.emitterNode.killParticle(particleData);
+                            emitterNode.killParticle(particleData);
                             break;
                         }
                     }
                 }
+
             } catch (final Exception ex) {
                 ex.printStackTrace();
             }
@@ -217,9 +254,14 @@ public class PhysicsInfluencer extends AbstractParticleInfluencer {
      * @param tpf          the tpf.
      */
     private void updateCollisionShape(@NotNull final ParticleData particleData, final float tpf) {
+
+        final ParticleEmitterNode emitterNode = particleData.getEmitterNode();
+        final Vector3f translation = tempVec.set(particleData.position)
+                .addLocal(emitterNode.getLocalTranslation());
+
         final Vector3f angles = particleData.angles;
         quat.fromAngles(angles.x, angles.y, angles.z);
-        geom.setLocalTranslation(particleData.position.add(particleData.emitterNode.getLocalTranslation()));
+        geom.setLocalTranslation(translation);
         geom.setLocalRotation(quat);
         geom.setLocalScale(particleData.size);
         geom.updateLogicalState(tpf);
@@ -241,6 +283,15 @@ public class PhysicsInfluencer extends AbstractParticleInfluencer {
         }
 
         geometries.add(geometry);
+    }
+
+    /**
+     * Remove a last collidable geometry.
+     */
+    public void removeLast() {
+        final int size = geometries.size();
+        if (size < 1) return;
+        removeCollidable(geometries.get(size - 1));
     }
 
     /**
@@ -351,8 +402,8 @@ public class PhysicsInfluencer extends AbstractParticleInfluencer {
     /**
      * This method clones the influencer instance.
      *
-     * ** Please note the geometry list is specific to each instance of the physics influencer and
-     * must be maintained by the user.  This list is NOT cloned from the original influencer.
+     * ** Please note the geometry list is specific to each instance of the physics influencer and must be maintained by
+     * the user.  This list is NOT cloned from the original influencer.
      */
     @NotNull
     @Override
